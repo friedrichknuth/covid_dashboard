b0196f22bde42e293be4359cc0cac22eff1dca02
Friedrich Knuth <friedrichknuth@users.noreply.github.com>
2021-10-03 19:04:52 -0700

    Update panelserverextension.py

--- panelserverextension.py
+++ panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the notebook panel app with bokeh server"""
-    Popen(["panel", "serve", "--show", "--port", "5009", "covid_interactive.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "--show", "--port", "5006", "covid-interactive.ipynb", "--allow-websocket-origin=*"])
